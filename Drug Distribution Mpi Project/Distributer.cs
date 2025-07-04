10577827fb6bd9a8039b5a7015434f11cafdedd1
TimaDawaa <tima302t@gmail.com>
2025-07-04 10:43:48 +0300

    Distributor and call in RoleAssignHelper

--- Drug Distribution Mpi Project/Distributer.cs
+++ Drug Distribution Mpi Project/Distributer.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MPI;
+using System;
+using System.Threading;
 
 namespace Drug_Distribution_Mpi_Project
 {
-    class Distributor
+    public static class Distributor
     {
+        public static void Run(int provinceIndex, Intracommunicator provinceComm, InputData input)
+        {
+            int rank = provinceComm.Rank;
+            int size = provinceComm.Size;
+
+            Console.WriteLine($"[Distributor Rank {rank} | Province {provinceIndex}] Starting distributor process.");
+
+            while (true)
+            {
+                var task = ReceiveTask(provinceComm);
+
+                if (task == null)
+                {
+                    Console.WriteLine($"[Distributor Rank {rank}] No more tasks. Exiting.");
+                    break;  
+                }
+
+                Console.WriteLine($"[Distributor Rank {rank}] Processing order {task.OrderId}...");
+                Thread.Sleep(input.AvgDeliveryTime * 1000); 
+
+                Console.WriteLine($"[Distributor Rank {rank}] Finished order {task.OrderId}.");
+
+                provinceComm.Send(task.OrderId, 0, 1); 
+            }
+        }
+
+        private class DeliveryTask
+        {
+            public int OrderId { get; set; }
+        }
+
+        private static DeliveryTask ReceiveTask(Intracommunicator provinceComm)
+        {
+          
+            if (provinceComm.ImmediateProbe(0, 0))
+            {
+                int orderId = provinceComm.Receive<int>(0, 0);
+                return new DeliveryTask { OrderId = orderId };
+            }
+            else
+            {
+             
+                return null;
+            }
+        }
     }
 }
--- Drug Distribution Mpi Project/Helper/RoleAssignHelper.cs
+++ Drug Distribution Mpi Project/Helper/RoleAssignHelper.cs
@@ -1,25 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MPI;
-
 
 namespace Drug_Distribution_Mpi_Project.Helper
 {
     class RoleAssignHelper
     {
-        public static void AssignAndRun(Intracommunicator world, int rank, InputData input)
+        public static void AssignAndRun(Intracommunicator worldComm, int rank, InputData input)
         {
-            int numProvinces = input.NumOfProvinces;
-            if (rank <= numProvinces)
+            int totalProvinces = input.NumOfProvinces;
+            int ranksPerProvince = input.DistributorsPerProvince + 1; 
+
+            int provinceIndex = (rank - 1) / ranksPerProvince;
+
+            if (provinceIndex < 0 || provinceIndex >= totalProvinces)
             {
-                //Province.Run(world, rank, input);
+                Console.WriteLine($"[Rank {rank}] has no assigned role.");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[Rank {rank}] → Belongs to province number {provinceIndex}");
+            Console.ResetColor();
+
+            Intracommunicator provinceComm = (Intracommunicator)worldComm.Split(provinceIndex, rank);
+
+            int localRank = provinceComm.Rank;
+
+            if (localRank == 0)
+            {
+                Province.RunAsLeader(provinceIndex, provinceComm, worldComm, input);
             }
             else
             {
-                //Distributor.Run(world, rank, input);
+                Distributor.Run(provinceIndex, provinceComm, input);
+
             }
         }
     }
