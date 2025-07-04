09283b7d9f2e835e51f4a231272379379f0dd6ac
sara najati <saranajatilaptop@gmail.com>
2025-07-04 10:22:49 +0300

    simulate class

--- Drug Distribution Mpi Project/simulate.cs
+++ Drug Distribution Mpi Project/simulate.cs
@@ -5,6 +5,34 @@ namespace Drug_Distribution_Mpi_Project
 {
     public static class Simulation
     {
-        
+        public static void AssignRole(Intracommunicator worldComm, int rank, InputData input)
+        {
+            int totalProvinces = input.NumOfProvinces;
+            int ranksPerProvince = input.DistributorsPerProvince + 1; //the additional 1 is for the leader
+
+            int provinceIndex = (rank - 1) / ranksPerProvince; //the province number which this rank belongs
+            if (provinceIndex >= totalProvinces)
+            {
+                Console.WriteLine($"[Rank {rank}] have no role");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[Rank {rank}] → Belongs to province number {provinceIndex}");
+            Console.ResetColor();
+
+            int color = provinceIndex;
+            Intracommunicator provinceComm = (Intracommunicator)worldComm.Split(color, rank); //split is for creating a sub-communicator for each province
+
+            int localRank = provinceComm.Rank; 
+            if (localRank == 0) // head of province is 0
+            {
+                Province.RunAsLeader(provinceIndex, provinceComm, worldComm, input);
+            }
+            else
+            {
+                //Distributor 
+            }
+        }
     }
 }
